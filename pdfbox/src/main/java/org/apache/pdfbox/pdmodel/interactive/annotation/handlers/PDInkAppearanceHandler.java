1ce69e5911b26d0ba7b461f71de2b07692175dcc
Jungsoo Yang <jungsoo827@gmail.com>
2020-05-25 16:13:25 +0900

    updated to add new point for ink appearance if difference between 2 points are greater than 5.0

--- pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDInkAppearanceHandler.java
+++ pdfbox/src/main/java/org/apache/pdfbox/pdmodel/interactive/annotation/handlers/PDInkAppearanceHandler.java
@@ -32,66 +32,55 @@ import org.apache.pdfbox.pdmodel.PDDocument;
 
 /**
  * Handler to generate the ink annotations appearance.
- *
  */
-public class PDInkAppearanceHandler extends PDAbstractAppearanceHandler
-{
+public class PDInkAppearanceHandler extends PDAbstractAppearanceHandler {
     private static final Log LOG = LogFactory.getLog(PDInkAppearanceHandler.class);
 
-    public PDInkAppearanceHandler(PDAnnotation annotation)
-    {
+    public PDInkAppearanceHandler(PDAnnotation annotation) {
         super(annotation);
     }
 
-    public PDInkAppearanceHandler(PDAnnotation annotation, PDDocument document)
-    {
+    public PDInkAppearanceHandler(PDAnnotation annotation, PDDocument document) {
         super(annotation, document);
     }
 
     @Override
-    public void generateAppearanceStreams()
-    {
+    public void generateAppearanceStreams() {
         generateNormalAppearance();
         generateRolloverAppearance();
         generateDownAppearance();
     }
 
     @Override
-    public void generateNormalAppearance()
-    {
+    public void generateNormalAppearance() {
         PDAnnotationMarkup ink = (PDAnnotationMarkup) getAnnotation();
         // PDF spec does not mention /Border for ink annotations, but it is used if /BS is not available
         AnnotationBorder ab = AnnotationBorder.getAnnotationBorder(ink, ink.getBorderStyle());
         PDColor color = ink.getColor();
-        if (color == null || color.getComponents().length == 0 || Float.compare(ab.width, 0) == 0)
-        {
+        if (color == null || color.getComponents().length == 0 || Float.compare(ab.width, 0) == 0) {
             return;
         }
 
         PDAppearanceContentStream cs = null;
 
-        try
-        {
+        try {
             cs = getNormalAppearanceAsContentStream();
 
             setOpacity(cs, ink.getConstantOpacity());
 
             cs.setStrokingColor(color);
-            if (ab.dashArray != null)
-            {
+            if (ab.dashArray != null) {
                 cs.setLineDashPattern(ab.dashArray, 0);
             }
             cs.setLineWidth(ab.width);
 
             List<Point2D.Double> pointList = new ArrayList<Point2D.Double>();
             CubicSpline spline = new CubicSpline();
-            for (float[] pathArray : ink.getInkList())
-            {
+            for (float[] pathArray : ink.getInkList()) {
                 int nPoints = pathArray.length / 2;
                 // "When drawn, the points shall be connected by straight lines or curves
                 // in an implementation-dependent way" - we do lines.
-                for (int i = 0; i < nPoints; ++i)
-                {
+                for (int i = 0; i < nPoints; ++i) {
                     int index = i * 2;
                     float x1 = pathArray[index];
                     float y1 = pathArray[index + 1];
@@ -102,42 +91,81 @@ public class PDInkAppearanceHandler extends PDAbstractAppearanceHandler
 
                 if (pointList.size() > 2) {
                     spline.calcSpline();
-                    for(float f = 0; f<=1; f+=0.01) {
+                    Point2D.Double previousPoint1 = null;
+                    Point2D.Double previousPoint2 = null;
+                    for (float f = 0; f <= 1; f += 0.01) {
                         Point2D.Double p = spline.getPoint(f);
-                        Point2D.Double pnt = new Point2D.Double(p.x, p.y);
-                        System.out.print(f+":");
-//                        System.out.println(pnt.toString());
-                        if (f >0){
-                            cs.lineTo((float)pnt.getX(), (float)pnt.getY());
-                            System.out.println("lineTo:" +pnt.toString());
+                        if (f > 0) {
+                            if (previousPoint1 != null && previousPoint2 != null) {
+                                double originalDiffX1 = p.getX() - previousPoint1.getX();
+                                double originalDiffY1 = p.getY() - previousPoint1.getY();
+                                double originalDiffX2 = previousPoint1.getX() - previousPoint2.getX();
+                                double originalDiffY2 = previousPoint1.getY() - previousPoint2.getY();
+                                double diffX1 = originalDiffX1;
+                                double diffY1 = originalDiffY1;
+                                if (diffX1 < 0) {
+                                    diffX1 *= -1;
+                                }
+                                if (diffY1 < 0) {
+                                    diffY1 *= -1;
+                                }
+                                if (diffX1 > 5 && diffY1 > 5) {
+                                    Point2D.Double tempPoint = new Point2D.Double(0, 0);
+                                    double nextOffsetX = originalDiffX1 / 5.0;
+                                    double nextOffsetY = originalDiffY1 / 5.0;
+                                    if (originalDiffX1 < 0 && originalDiffX2 > 0){
+                                        tempPoint.x = previousPoint1.getX() + nextOffsetX;
+                                    } else if (originalDiffX1 > 0 && originalDiffX2 < 0){
+                                        tempPoint.x = previousPoint1.getX() + nextOffsetX;
+                                    } else if (originalDiffX1 > 0 && originalDiffX2 > 0){
+                                        tempPoint.x = previousPoint1.getX() + nextOffsetX;
+                                    } else if (originalDiffX1 < 0 && originalDiffX2 < 0){
+                                        tempPoint.x = previousPoint1.getX() + nextOffsetX;
+                                    }
+
+                                    if (originalDiffY1 < 0 && originalDiffY2 > 0){
+                                        tempPoint.y = previousPoint1.getY() + nextOffsetY;
+                                    } else if (originalDiffY1 > 0 && originalDiffY2 < 0){
+                                        tempPoint.y = previousPoint1.getY() + nextOffsetY;
+                                    } else if (originalDiffY1 > 0 && originalDiffY2 > 0){
+                                        tempPoint.y = previousPoint1.getY() + nextOffsetY;
+                                    } else if (originalDiffY1 < 0 && originalDiffY2 < 0){
+                                        tempPoint.y = previousPoint1.getY() + nextOffsetY;
+                                    }
+                                    if (tempPoint.getX() > 0) {
+                                        cs.lineTo((float) tempPoint.getX(), (float) tempPoint.getY());
+                                        LOG.debug(f + ":lineTo:" + tempPoint.toString());
+                                    }
+                                }
+                            }
+                            cs.lineTo((float) p.getX(), (float) p.getY());
+                            LOG.debug(f + ":lineTo:" + p.toString());
                         } else {
-                            cs.moveTo((float)pnt.getX(), (float)pnt.getY());
-                            System.out.println("moveTo:" +pnt.toString());
+                            cs.moveTo((float) p.getX(), (float) p.getY());
+                            LOG.debug("moveTo:" + p.toString());
                         }
+                        if (previousPoint1 != null){
+                            previousPoint2 = previousPoint1;
+                        }
+                        previousPoint1 = p;
                     }
                     cs.stroke();
                 }
             }
-        }
-        catch (IOException ex)
-        {
+        } catch (IOException ex) {
             LOG.error(ex);
-        }
-        finally
-        {
+        } finally {
             IOUtils.closeQuietly(cs);
         }
     }
 
     @Override
-    public void generateRolloverAppearance()
-    {
+    public void generateRolloverAppearance() {
         // No rollover appearance generated
     }
 
     @Override
-    public void generateDownAppearance()
-    {
+    public void generateDownAppearance() {
         // No down appearance generated
     }
 }
